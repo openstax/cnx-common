a8d5be9917842d00a1b689d9a925789e2852bf44
BryanEliDimas <bryanelidimas@gmail.com>
2019-04-26 09:50:21 -0500

    Generate slug URLs using slugify

--- cnxcommon/urlslug.py
+++ cnxcommon/urlslug.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import re
 
-from unidecode import unidecode
+from slugify import slugify
 
 
 def generate_slug(title):
@@ -11,38 +11,5 @@ def generate_slug(title):
     GIVEN "1.1 The Science of Biology"
     RETURNS  "1-1-the-science-of-biology"
     """
-    section_number = extract_section_number(title)
-    title = split_parts(title)[1].strip().lower()
-
     title = re.sub(r"<.*?>", "", title)  # remove html tags
-    import html  # PY3
-    # unescape html chars like '&amp;' becomes '&'
-    title = html.unescape(title)
-
-    title = unidecode(title)  # replace unicode chars with ascii equivalents
-
-    title = re.sub(r'[^a-z0-9 ]', ' ', title)  # remove special characters
-    title = re.sub(r' +', '-', title)  # spaces to dashes
-    title = title.strip("-")  # remove trailing dashes
-
-    slug = f"{section_number}-{title}" if section_number else f"{title}"
-    return slug
-
-
-def extract_section_number(complete_title):
-    section_number = split_parts(complete_title)[0]
-    # use dash instead of '.'
-    section_number = re.sub(r'\.', '-', section_number)
-
-    return section_number
-
-
-def split_parts(complete_title):
-    from copy import copy
-    title = copy(complete_title)
-    matches = re.findall(r'^([0-9\.]*)?(.*)$', title)
-
-    section_number = matches[0][0]
-    title = matches[0][1]
-
-    return [section_number, title]
+    return slugify(title)
