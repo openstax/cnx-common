adde14577adab68bd6de5ec47bc5ef925de00e4a
BryanEliDimas <bryanelidimas@gmail.com>
2019-04-29 15:37:42 -0500

    Slug generator - return string for book OR section title
    Previously, the slug generator returned a tuple with 1-2 items... the book title slug AND the section title slug.
    
    After these changes, it always returns a string and uses the second item only if the complete section title slug can't be built because it's missing the chapter title in the 3rd parameter.

--- cnxcommon/urlslug.py
+++ cnxcommon/urlslug.py
@@ -4,55 +4,48 @@ import re
 from slugify import slugify
 
 
-def generate_slug(book_title, section_title="", chapter_title=""):
-    """Given a book title, a section title, and a chapter title
-    generates slugs for the book title and the section title.
+def generate_slug(book_title, *other_titles):
+    """Generates a slug for a book title or a section title.
 
     GIVEN just the book title
-    book_title = "1.1 The Science of Biology"
+    book_title = "College Physics"
     RETURNS
-     "1-1-the-science-of-biology"
+     "college-physics"
 
     GIVEN
     book_title = "College Physics"
-    section_title = "1.1 The Science of Biology"
-    RETURNS a tuple
-     ("college-physics", "1-1-the-science-of-biology")
+    other_title = "1.1 The Science of Biology"
+    RETURNS
+     "1-1-the-science-of-biology"
 
      GIVEN
      book_title = "College Physics"
-     chapter_title = "1 Introduction: The Nature of Science and Physics"
-     section_title = "Problems &amp; Exercises"
-     RETURNS a tuple
-     ("college-physics", "1-problems-and-exercises")
+     other_title[0] = "1 Introduction: The Nature of Science and Physics"
+     other_title[1] = "Problems &amp; Exercises"
+     RETURNS
+     "1-problems-exercises"
 
      NOTE that the chapter title is only used to determine the chapter number
-     in case it is missing from the section title like for "Introduction" sections.
+     in case it is missing from the section title - like for "Introduction" sections.
     """
-    if chapter_title and not section_title:
-        """The chapter title is ONLY used for generating a complete section title,
-        so just the chapter title is useless, please check for mistakes.
-        """
-        return (None, None)
+    if len(other_titles) == 0:
+        book_title = slugify(remove_html_tags(book_title))
+        return book_title
 
-    book_title = slugify(remove_html_tags(book_title))
+    section_title = slugify(remove_html_tags(other_titles[-1]))
 
-    # allow generating a slug when only one argument is passed-in, essentially.
-    if not section_title :
-        return (book_title,)
-
-    section_title = slugify(remove_html_tags(section_title))
     if re.match(r"^\d", section_title):  # if section title starts with a digit
-        pass  # we already know the chapter number
-    else:  # prepend the chapter number to the section title
-        chapter_title = remove_html_tags(chapter_title)
-        # find chapter number in chapter title
-        chapter_number = re.split(r"^([0-9\.]*)?(.*)$", chapter_title)[1]
-        # update section title with the chapter number, only if found.
-        w_chapter_number = "{}-{}".format(chapter_number, section_title)
-        section_title = w_chapter_number if chapter_number else "{}".format(section_title)
+        # we must already have the chapter and section numbers
+        return section_title
+    else:  # find the chapter number
+        try:
+            chapter_title = remove_html_tags(other_titles[-2])
+            chapter_number = re.split(r"^([0-9\.]*)?(.*)$", chapter_title)[1]
+            section_title = "{}-{}".format(chapter_number, section_title) if chapter_number else section_title
+        except IndexError:
+            pass  # chapter title not present
 
-    return (book_title, section_title)
+        return section_title
 
 
 def remove_html_tags(title):
--- tests/test_urlslug.py
+++ tests/test_urlslug.py
@@ -17,7 +17,7 @@ class TestSlugGenerator:
         expected = "sometext"
         actual = generate_slug(title)
 
-        assert expected == actual[0]
+        assert expected == actual
 
     def test_it_removes_special_chars(self):
         title = "@#$*(&sometext!!!"
@@ -25,8 +25,8 @@ class TestSlugGenerator:
         expected = "sometext"
         actual = generate_slug(title)
 
-        assert re.match(r'^\W+$', actual[0]) is None
-        assert expected == actual[0]
+        assert re.match(r'^\W+$', actual) is None
+        assert expected == actual
 
     def test_it_replaces_special_chars_btwn_text_with_dashes(self):
         title = "@#$*(&some!!!text!!!"
@@ -34,66 +34,66 @@ class TestSlugGenerator:
         expected = "some-text"
         actual = generate_slug(title)
 
-        assert re.match(r'^\W+$', actual[0]) is None
-        assert expected == actual[0]
+        assert re.match(r'^\W+$', actual) is None
+        assert expected == actual
 
     def test_it_replaces_spaces_with_dashes(self):
         title = "some text and     a    bunch      of     spaces    "
         expected = "some-text-and-a-bunch-of-spaces"
         actual = generate_slug(title)
 
-        assert expected == actual[0]
+        assert expected == actual
 
     def test_it_makes_all_lowercase(self):
         title = "SomeTEXT"
         expected = "sometext"
         actual = generate_slug(title)
 
-        assert expected == actual[0]
+        assert expected == actual
 
     def test_it_replaces_unicode_chars_w_their_ascii_equivalent(self):
         title = "podręcznikfizykadlaszkółwyższych"
         expected = "podrecznikfizykadlaszkolwyzszych"
         actual = generate_slug(title)
 
-        assert expected == actual[0]
+        assert expected == actual
 
     def test_it_can_identify_chapter_and_section_numbers(self):
         title = "12.4 sometext"
         expected = "12-4-sometext"
         actual = generate_slug(title)
 
-        assert expected == actual[0]
+        assert expected == actual
 
     def test_with_only_chapter_number_not_section(self):
         title = "1 introduction"
         expected = "1-introduction"
         actual = generate_slug(title)
 
-        assert expected == actual[0]
+        assert expected == actual
 
     def test_using_generate_slug(self):
         title = "12.4 sometext"
         expected = "12-4-sometext"
         actual = generate_slug(title)
 
-        assert expected == actual[0]
+        assert expected == actual
 
     def test_it_removes_trailing_slashes(self):
         title = "-12.4 some-text--"
         expected = "12-4-some-text"
         actual = generate_slug(title)
 
-        assert expected == actual[0]
+        assert expected == actual
 
     def test_it_removes_html_encoded_chars(self):
         title = "12.4 sometext&amp;moretext"
         expected = "12-4-sometext-moretext"
         actual = generate_slug(title)
 
-        assert expected == actual[0]
+        assert expected == actual
 
-    def test_it_can_identify_chapter_num_when_not_in_section_title(self):
+    def test_it_can_find_chapter_number_when_not_in_section_title(self):
         """If the chapter number isn't present in the section title,
         it can find it in the chapter title instead.
         """
@@ -101,9 +101,9 @@ class TestSlugGenerator:
         chapter_title = "1 Introduction: The Nature of Science and Physics"
         section_title = "problems-and-exercises"
         expected = "1-problems-and-exercises"
-        actual = generate_slug(book_title, chapter_title=chapter_title, section_title=section_title)
+        actual = generate_slug(book_title, chapter_title, section_title)
 
-        assert expected == actual[1]
+        assert expected == actual
 
     def test_when_chapter_number_not_present_in_section_nor_chapter_titles(self):
         """What happens when (or if) a chapter number is not present in the chapter
@@ -113,39 +113,41 @@ class TestSlugGenerator:
         chapter_title = "Introduction: The Nature of Science and Physics"
         section_title = "problems-and-exercises"
         expected = "problems-and-exercises"
-        actual = generate_slug(
-            book_title, chapter_title=chapter_title, section_title=section_title)
+        actual = generate_slug(book_title, chapter_title, section_title)
 
-        assert expected == actual[1]
+        assert expected == actual
 
-    def test_slug_generator_integration_test(self):
-        """Integration-level test using the Acceptance Criteria described in:
+    def test_slug_generator_acceptance_test(self):
+        """High-level test using the Acceptance Criteria described in:
         https://github.com/openstax/cnx/issues/348
         """
         books = [
             (
                 'College Physics',
-                '<span class="os-number">12.4</span><span class="os-divider"> </span><span class="os-text">Viscosity and Laminar Flow; Poiseuille\'s Law< /span >',
                 '<span class="os-number">12</span><span class="os-divider"> </span><span class="os-text">Fluid Dynamics and Its Biological and Medical Applications</span>',
+                '<span class="os-number">12.4</span><span class="os-divider"> </span><span class="os-text">Viscosity and Laminar Flow; Poiseuille\'s Law< /span >',
             ), (
                 'College Physics',
-                '<span class="os-text">Problems &amp; Exercises</span>',
                 '<span class="os-number">1</span><span class="os-divider"> </span><span class="os-text">Introduction: The Nature of Science and Physics</span>',
+                '<span class="os-text">Problems &amp; Exercises</span>',
             ), (
                 'Biology 2e',
-                '<span class="os-number">1.1</span><span class="os-divider"> </span><span class="os-text">The Science of Biology</span>',
                 '<span class="os-number">1</span><span class="os-divider"> </span><span class="os-text">The Study of Life</span>',
+                '<span class="os-number">1.1</span><span class="os-divider"> </span><span class="os-text">The Science of Biology</span>',
             ), (
                 'Biology 2e',
                 '<span class="os-text">Preface</span>',
+            ), (
+                'Biology 2e',
             ),
         ]
 
         expectations = [
-            ('college-physics', '12-4-viscosity-and-laminar-flow-poiseuille-s-law'),
-            ('college-physics', '1-problems-exercises'),
-            ('biology-2e', '1-1-the-science-of-biology'),
-            ('biology-2e', 'preface')
+            '12-4-viscosity-and-laminar-flow-poiseuille-s-law',
+            '1-problems-exercises',
+            '1-1-the-science-of-biology',
+            'preface',
+            'biology-2e',
         ]
 
         for index, book in enumerate(books):
